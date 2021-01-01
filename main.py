b85c41593b86cb122fbc6e7067c29771891e0df5
Eldad Horvitz <eldad373@gmail.com>
2021-01-01 15:23:34 +0200

    initialize array work

--- main.py
+++ main.py
@@ -31,7 +31,18 @@ def train(epoch, model,optimizer,train_loader):
         loss = F.nll_loss(output, labels, reduction =="sum")
         loss.backward()
         optimizer.step()
-
+def test(epoch, model,optimizer,test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).cpu().sum()
+    test_loss /= len(test_loader.dataset)
+    print(’\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n’.format(test_loss, correct, len(test_loader.dataset),100. * correct / len(test_loader.dataset)))
 x, y, testx, testy = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 x = np.loadtxt(x)
 y = np.loadtxt(y)
